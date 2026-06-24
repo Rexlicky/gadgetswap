4b5052e06ebb2c6fb15175be4ed4b96cc31ca9eb
Rexlicky Verdhika Sagatha <sagathaoke@gmail.com>
2026-06-24 17:00:01 +0700

    feat: move navbar to global layout

--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,6 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <main className="relative min-h-screen overflow-hidden bg-black text-white">
-      {/* =========================
-          NAVBAR
-      ========================== */}
-      <Navbar />
 
       {/* =========================
           AURORA BACKGROUND
